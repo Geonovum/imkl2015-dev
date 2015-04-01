8c72d9d7b734f52828bb6c47659d7602ca6b632e
Wilko Quak <wilkoquak@gmail.com>
2015-04-01 11:25:57 +0200

    initial version from Belgium

---
+++ voorbeeldbestanden-belgie/doit.sh
@@ -0,0 +1,16 @@
+unzip basispakket.zip 
+unzip basispakket1Appurtenance.zip 
+unzip container\ -\ single\ theme.zip 
+unzip pakket\ met\ ActivityComplex.zip 
+unzip pakket\ met\ ExtraInformatie.zip 
+unzip pakket\ met\ ExtraTopografie.zip 
+unzip pakket\ met\ dieptes.zip 
+mkdir basispakket
+mkdir basispakket1Appurtenance
+mkdir pakket-met-ActivityComplex
+mkdir pakket-met-dieptes
+mkdir pakket-met-ExtraTopografie
+mkdir pakket-met-ExtraTopografie
+mkdir pakket-met-ExtraInformatie
+mkdir container-single-theme
+ 
