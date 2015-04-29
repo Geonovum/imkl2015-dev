5d95a65279c5361532cb49755a0551fd182bbfcb
Wilko Quak <c.w.quak@tudelft.nl>
2015-04-29 16:21:56 +0200

    New objecten toegevoegd.

--- voorbeelddata-paul/sql2gml/postprocess.sql
+++ voorbeelddata-paul/sql2gml/postprocess.sql
@@ -1,15 +1,233 @@
-alter table ms_kabelofleiding drop column if exists geometryid;
-alter table ms_kabelofleiding add column geometryid text;
-update ms_kabelofleiding set geometryid = 'geomkabelofleiding' || gid ;
+--
+-- Elektriciteitskabel
+-- 
+drop table if exists elektriciteitskabel;
+create table elektriciteitskabel
+as
+   select 
+	gid,
+	id::text,
+	thema::text,
+	netbeheer::text,
+	status::text,
+	vertpositi::text,
+	warningt::text,
+	opvolt::text,
+	nomvolt::text,
+	toelichtin::text,
+	dieptetovm::text,
+	dieptenap::text,
+	xinfo::text,
+	disttype::text,
+	geom,
+	geometryid::text,
+	linkid::text,
+        'hs' || gid as gmlid
+     from hs_kabelofleiding
+   union all
+   select
+	gid,
+	id::text,
+	thema::text,
+	netbeheer::text,
+	status::text,
+	vertpositi::text,
+	warningt::text,
+	opvolt::text,
+	null::text,
+	toelichtin::text,
+	dieptetovm::text,
+	dieptenap::text,
+	xinfo::text,
+	disttype::text,
+	geom,
+	geometryid::text,
+	linkid::text,
+        'ls' || gid as gmlid
+      from ls_kabelofleiding
+   union all
+   select 
+	gid,
+	id::text,
+	thema::text,
+	netbeheer::text,
+	status::text,
+	vertpositi::text,
+	warningt::text,
+	opvolt::text,
+	nomvolt::text,
+	toelichtin::text,
+	dieptetovm::text,
+	dieptenap::text,
+	xinfo::text,
+	disttype::text,
+	geom,
+	geometryid::text,
+	linkid::text,
+       'ms' || gid as gmlid
+       from ms_kabelofleiding
+;
 
-alter table leidingelement drop column if exists geometryid;
-alter table leidingelement add column geometryid text;
-update leidingelement set geometryid = 'geomleidingelement' || gid ;
+alter table elektriciteitskabel drop column if exists linkid;
+alter table elektriciteitskabel add column linkid text;
+update elektriciteitskabel set linkid = 'hs_kabel0fleiding-' || gid ;
+
+update elektriciteitskabel 
+   set nomvolt = 0.0 where nomvolt is null;
+update elektriciteitskabel 
+   set opvolt = 0.0 where opvolt is null;
+
+--
+-- OlieGasChemicalien
+-- 
+drop table if exists oliegaschemicalien;
+create table oliegaschemicalien
+as
+    select
+	 gid,
+	 id,
+	 thema,
+	 netbeheer,
+	 status,
+	 vertpositi,
+	 disttype,
+	 warningt,
+	 pipediam::text,
+	 pressure,
+	 producttyp,
+	 toelichtin,
+	 dieptetovm,
+	 dieptenap,
+	 xinfo,
+	 geom
+    from hd_gas_kabelofleiding
+    union all
+    select
+	 gid,
+	 id,
+	 thema,
+	 netbeheer,
+	 status,
+	 vertpositi,
+	 disttype,
+	 warningt,
+	 pipediam::text,
+	 pressure,
+	 producttyp,
+	 toelichtin,
+	 dieptetovm,
+	 dieptenap,
+	 xinfo,
+	 geom
+    from ld_gas_kabelofleiding
+    union all
+    select
+	 gid,
+	 id,
+	 thema,
+	 netbeheer,
+	 status,
+	 vertpositi,
+	 disttype,
+	 warningt,
+	 pipediam::text,
+	 pressure,
+	 producttyp,
+	 toelichtin,
+	 dieptetovm,
+	 dieptenap,
+	 xinfo,
+	 geom
+    from bgevaarlijkeinhoud
+;
+alter table oliegaschemicalien drop column if exists linkid;
+alter table oliegaschemicalien add column linkid text;
+update oliegaschemicalien set linkid = 'oliegas-' || gid ;
+update oliegaschemicalien set pipediam = '0.0' where pipediam is null ;
+update oliegaschemicalien set pressure = '0.0' where pressure is null ;
+
+
+--
+--
+--
+drop table if exists kabelb;
+create table kabelb
+as
+    select 
+	 gid        , 
+	 id         , 
+	 thema      , 
+	 type       , 
+	 netbeheer  , 
+	 status     , 
+	 vertpositi , 
+	 disttype   , 
+	 warningt   , 
+	 ductwidth  , 
+	 bzichtbaar , 
+	 toelichtin , 
+	 aantalk    , 
+	 dieptetovm , 
+	 dieptenap  , 
+	 xinfo      , 
+	 geom
+    from kabelbed
+union all
+    select 
+	 gid        , 
+	 id         , 
+	 thema      , 
+	 type       , 
+	 netbeheer  , 
+	 status     , 
+	 vertpositi , 
+	 disttype   , 
+	 warningt   , 
+	 ductwidth  , 
+	 bzichtbaar , 
+	 toelichtin , 
+	 aantalk    , 
+	 dieptetovm , 
+	 dieptenap  , 
+	 xinfo      , 
+	 geom 
+    from dt_kabelbed;
+
+
+alter table kabelb drop column if exists linkid;
+alter table kabelb add column linkid text;
+update kabelb set linkid = 'kabelbed-' || gid ;
+
+
+
+
+
+
+alter table rioolvv_kabelofleiding drop column if exists linkid;
+alter table rioolvv_kabelofleiding add column linkid text;
+update rioolvv_kabelofleiding set linkid = 'rioolvv_kabelofleiding' || gid ;
+
+alter table water_kabelofleiding drop column if exists linkid;
+alter table water_kabelofleiding add column linkid text;
+update water_kabelofleiding set linkid = 'water_kabelofleiding' || gid ;
+
+alter table leidingelement drop column if exists linkid;
+alter table leidingelement add column linkid text;
+update leidingelement set linkid = 'leidingelement' || gid ;
+
+alter table leidingelement add column unetid text;
+update leidingelement l
+   set unetid = 'unetid-' || (select gid from utiliteitsnet u where u.thema = l.thema);
 
 drop table if exists utilitylink;
 create table utilitylink
 as
-    select gid,thema,geom,geometryid
-    from ms_kabelofleiding
+    select gid,thema,geom,linkid,status::text
+    from elektriciteitskabel
+       union all
+    select gid,thema,geom,linkid,status::text
+    from rioolvv_kabelofleiding
+       union all
+    select gid,thema,geom,linkid,status::text
+    from water_kabelofleiding
 ;
-
--- voorbeelddata-paul/sql2gml/sql2gml.php
+++ voorbeelddata-paul/sql2gml/sql2gml.php
@@ -5,8 +5,11 @@
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xmlns:imkl="http://www.geonovum.nl/wion/2015/1.0"
     xmlns:net="urn:x-inspire:specification:gmlas:Network:3.2"
+    xmlns:us-net-ogc="http://inspire.ec.europa.eu/schemas/us-net-ogc/3.0"
     xmlns:xlink="http://www.w3.org/1999/xlink"
+    xmlns:base="urn:x-inspire:specification:gmlas:BaseTypes:3.2"
     xmlns:us-net-common="http://inspire.ec.europa.eu/schemas/us-net-common/3.0"
+    xmlns:us-net-el="http://inspire.ec.europa.eu/schemas/us-net-el/3.0"
     gml:id="ID_1c0c5554-5c4a-467a-a9ef-9f36b5af2bfq"
     xsi:schemaLocation="http://www.geonovum.nl/wion/2015/1.0 ../../xsd/IMKL2015-wion.xsd">
 
@@ -27,7 +30,7 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "    <gml:featureMember>\n";
-    echo "        <imkl:Utiliteitsnet gml:id=\"ID" . $line["id"] . uniqid() . "\">\n";
+    echo "        <imkl:Utiliteitsnet gml:id=\"unetid" . $line["gid"] . uniqid() . "\">\n";
     echo "        <us-net-common:utilityNetworkType xlink:href=\"" . $line["unetworkty"] . "\"/>\n";
 #
 # TODO add authorityRole
@@ -53,16 +56,16 @@ pg_free_result($result);
 //
 // Process leidingelement
 //
-$query = 'select gid,geometryid,id,thema,netbeheer,type,status,vertpositi,bzichtbaar,dieptetovm,dieptenap,xinfo,hoogte,detailsch,ST_AsGML(3,geom,5,0,null,geometryid) as geom from leidingelement ;';
+$query = 'select gid,unetid,linkid,id,thema,netbeheer,type,status,vertpositi,bzichtbaar,dieptetovm,dieptenap,xinfo,hoogte,detailsch,ST_AsGML(3,geom,5,0,null,linkid || \'x\') as geom from leidingelement ;';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "    <gml:featureMember>\n";
-    echo "        <imkl:Appurtenance gml:id=\"ID" . $line["geometryid"] . "\">\n";
+    echo "        <imkl:Appurtenance gml:id=\"ID" . $line["linkid"] . "\">\n";
     echo "            <net:beginLifespanVersion>2001-12-17T09:30:47.0Z</net:beginLifespanVersion>\n";
 #    echo "            <imkl:bovengrondsZichtbaar>" . $line["bzichtbaar"] . "\n";
 #    echo "            </imkl:bovengrondsZichtbaar>\n";
-    echo "            <net:inNetwork xlink:href=\"" . $line["thema"] . "\"/>\n";
+    echo "            <net:inNetwork xlink:href=\"" . $line["unetid"] . "\"/>\n";
     echo "            <net:geometry>" . $line["geom"] . "\n";
     echo "            </net:geometry>\n";
     echo "            <us-net-common:currentStatus xlink:href=\"" . $line["status"] ."\"/>\n";
@@ -75,21 +78,57 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 }
 pg_free_result($result);
 
-//
-// Process utilitylink
-//
-$query = 'select gid,thema,geometryid,ST_AsGML(3,geom,5,0,null,geometryid) as geom from utilitylink ;';
+#
+# Process oliegaschemicalien
+#
+$query = 'select gid,id,thema,netbeheer,status,vertpositi,producttyp,warningt,toelichtin,pressure,pipediam,dieptetovm,dieptenap,xinfo,disttype,linkid from oliegaschemicalien ;';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "    <gml:featureMember>\n";
-    echo "        <us-net-common:UtilityLink gml:id=\"IDms_kabelofleiding" . $line["gid"] . "\">\n";
+    echo "        <imkl:OlieGasChemicalienPijpleiding gml:id=\"IDms_kabelofleiding" . $line["gid"] . "\">\n";
+    echo "            <net:beginLifespanVersion>2001-12-17T09:30:47.0Z</net:beginLifespanVersion>\n";
+    echo "            <net:inspireId>\n";
+    echo "                <base:Identifier>\n";
+    echo "                <base:localId>" . $line["gid"] . "</base:localId>\n";
+    echo "                <base:namespace>gascom-be</base:namespace>\n";
+    echo "            </base:Identifier>\n";
+    echo "            </net:inspireId>\n";
+    echo "            <net:inNetwork xlink:href=\"" . $line["thema"] . "\"/>\n";
+    echo "            <net:link xlink:href=\"" . $line["linkid"] . "\"/>\n";
+    echo "            <us-net-common:currentStatus xlink:href=\"" . $line["status"] ."\"/>\n";
+    echo "            <us-net-common:validFrom>2001-12-17T09:30:47.0Z</us-net-common:validFrom>\n";
+    echo "            <us-net-common:validTo>2001-12-17T09:30:47.0Z</us-net-common:validTo>\n";
+    echo "            <us-net-common:verticalPosition>underground</us-net-common:verticalPosition>\n";
+    echo "            <us-net-common:utilityFacilityReference xlink:href=\"xxxxTODO\"/>\n";
+#echo "            <us-net-common:utilityDeliveryType xlink:href="http://inspire.ec.europa.eu/codelist/UtilityDeliveryTypeExtendedValue/distribution"/
+    echo "            <us-net-common:warningType xlink:href=\"http://inspire.ec.europa.eu/codelist/WarningTypeExtendedValue/net\"/>\n";
+    echo "            <us-net-common:pipeDiameter uom=\"urn:ogc:def:uom:OGC::cm\">" . $line["pipediam"] . "</us-net-common:pipeDiameter>\n";
+    echo "            <us-net-common:pressure uom=\"urn:ogc:def:uom:OGC::bar\">" . $line["pressure"] . "</us-net-common:pressure>\n";
+    echo "            <us-net-ogc:oilGasChemicalsProductType xlink:href=\"" .  $line["producttyp"] . "\" />\n";
+    echo "        </imkl:OlieGasChemicalienPijpleiding>\n";
+    echo "    </gml:featureMember>\n\n";
+}
+pg_free_result($result);
+//
+// Kabelbed
+//
+$query = 'select linkid,gid,id,thema,type,netbeheer,status,vertpositi,disttype,warningt,ductwidth,bzichtbaar,toelichtin,aantalk,dieptetovm,dieptenap,xinfo,geom from kabelb;';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "    <gml:featureMember>\n";
+    echo "        <imkl:Kabelbed gml:id=\"IDms_kabelofleiding" . $line["gid"] . "\">\n";
     echo "            <net:beginLifespanVersion>2001-12-17T09:30:47.0Z</net:beginLifespanVersion>\n";
     echo "            <net:inNetwork xlink:href=\"" . $line["thema"] . "\"/>\n";
-    echo "            <net:centrelineGeometry>" . $line["geom"] . "\n";
-    echo "            </net:centrelineGeometry>\n";
-    echo "            <net:fictitious>underground</net:fictitious>\n";
-    echo "        </us-net-common:UtilityLink>\n";
+    echo "            <net:link xlink:href=\"" . $line["linkid"] . "\"/>\n";
+    echo "            <us-net-common:currentStatus xlink:href=\"" . $line["status"] ."\"/>\n";
+    echo "            <us-net-common:validFrom>2001-12-17T09:30:47.0Z</us-net-common:validFrom>\n";
+    echo "            <us-net-common:validTo>2001-12-17T09:30:47.0Z</us-net-common:validTo>\n";
+    echo "            <us-net-common:verticalPosition>underground</us-net-common:verticalPosition>\n";
+    echo "            <us-net-common:warningType xlink:href=\"http://inspire.ec.europa.eu/codelist/WarningTypeExtendedValue/net\"/>\n";
+    echo "	      <us-net-common:ductWidth uom=\"urn:ogc:def:uom:OGC::cm\">" . $line["ductwidth"] ."</us-net-common:ductWidth>\n";
+    echo "        </imkl:Kabelbed>\n";
     echo "    </gml:featureMember>\n\n";
 }
 pg_free_result($result);
@@ -97,7 +136,7 @@ pg_free_result($result);
 //
 // Process ms_kabelofleiding
 //
-$query = 'select gid,id,thema,netbeheer,status,vertpositi,warningt,opvolt,nomvolt,toelichtin,dieptetovm,dieptenap,xinfo,disttype,ST_AsGML(3,geom,5,0,null,geometryid) as geom from ms_kabelofleiding ;';
+$query = 'select gid,id,thema,netbeheer,status,vertpositi,warningt,opvolt,nomvolt,toelichtin,dieptetovm,dieptenap,xinfo,disttype,linkid from elektriciteitskabel ;';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
@@ -107,17 +146,42 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
 #    echo "            <imkl:bovengrondsZichtbaar>" . $line["bzichtbaar"] . "\n";
 #    echo "            </imkl:bovengrondsZichtbaar>\n";
     echo "            <net:inNetwork xlink:href=\"" . $line["thema"] . "\"/>\n";
-    echo "            <net:geometry>" . $line["geom"] . "\n";
-    echo "            </net:geometry>\n";
+    echo "            <net:link xlink:href=\"" . $line["linkid"] . "\"/>\n";
     echo "            <us-net-common:currentStatus xlink:href=\"" . $line["status"] ."\"/>\n";
     echo "            <us-net-common:validFrom>2001-12-17T09:30:47.0Z</us-net-common:validFrom>\n";
     echo "            <us-net-common:validTo>2001-12-17T09:30:47.0Z</us-net-common:validTo>\n";
     echo "            <us-net-common:verticalPosition>underground</us-net-common:verticalPosition>\n";
-    echo "            <us-net-common:appurtenanceType xlink:href=\"\"/>\n";
+    echo "	      <us-net-common:warningType xsi:nil=\"true\" />\n";
+    echo "	      <us-net-el:operatingVoltage uom=\"urn:ogc:def:uom:OGC::V\">" . $line["opvolt"] ."</us-net-el:operatingVoltage>\n";
+    echo "	      <us-net-el:nominalVoltage uom=\"urn:ogc:def:uom:OGC::V\">" . $line["nomvolt"] ."</us-net-el:nominalVoltage>\n";
     echo "        </imkl:Elektriciteitskabel>\n";
     echo "    </gml:featureMember>\n\n";
 }
 pg_free_result($result);
+
+//
+// Process utilitylink
+//
+$query = 'select gid,status,thema,linkid,ST_AsGML(3,geom,5,0,null,linkid) as geom from utilitylink ;';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "    <gml:featureMember>\n";
+    echo "        <us-net-common:UtilityLink gml:id=\"IDms_kabelofleiding" . $line["gid"] . "\">\n";
+    echo "            <net:beginLifespanVersion>2001-12-17T09:30:47.0Z</net:beginLifespanVersion>\n";
+    echo "            <net:inNetwork xlink:href=\"" . $line["thema"] . "\"/>\n";
+    echo "            <net:centrelineGeometry>" . $line["geom"] . "\n";
+    echo "            </net:centrelineGeometry>\n";
+    echo "            <net:fictitious>false</net:fictitious>\n";
+    echo "            <us-net-common:currentStatus xlink:href=\"" . $line["status"] ."\"/>\n";
+    echo "            <us-net-common:validFrom>2001-12-17T09:30:47.0Z</us-net-common:validFrom>\n";
+    echo "            <us-net-common:validTo>2001-12-17T09:30:47.0Z</us-net-common:validTo>\n";
+    echo "	      <us-net-common:verticalPosition xsi:nil=\"true\" />\n";
+    echo "        </us-net-common:UtilityLink>\n";
+    echo "    </gml:featureMember>\n\n";
+}
+pg_free_result($result);
+
 
 // Closing connection
 pg_close($dbconn);
