8e84f0817e574a9ab3d3561f2d5e579cd3ca36ec
Wilko Quak <c.w.quak@tudelft.nl>
2015-05-27 09:30:40 +0200

    shapechange edits

--- ShapeChange/RunShapeChange.bat
+++ ShapeChange/RunShapeChange.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar ..\ShapeChange2.0.0\ShapeChange-2.0.0.jar -Dfile.encoding=UTF-8 -c IMKL2015.xml
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar C:\Users\cquak\SURFdrive\ShapeChange\ShapeChange2.0.0\ShapeChange-2.0.0.jar -Dfile.encoding=UTF-8 -c IMKL2015.xml
 
 
 pause
