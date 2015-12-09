689c12f7f802832a8a7bb98e4d35a607551df773
Wilko Quak <wilkoquak@gmail.com>
2015-12-09 17:33:32 +0100

    Making local copies of ShapeChange configurations files
    I am making local copies of ShapeChange configuration files so that I
    can regenerate a schema even if these files change on the server.

--- ShapeChange/RunShapeChange.bat
+++ ShapeChange/RunShapeChange.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar C:\Users\cquak\SURFdrive\ShapeChange\ShapeChange2.0.0\ShapeChange-2.0.0.jar -Dfile.encoding=UTF-8 -c IMKL2015.xml
+java -jar src\ShapeChange-2.0.1.jar -Dfile.encoding=UTF-8 -c IMKL2015.xml
 
 
 pause
