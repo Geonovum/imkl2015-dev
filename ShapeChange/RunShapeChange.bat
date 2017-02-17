38e66c69c29e96950bdae2f9f1fa218b9a043748
Wilko Quak <wilkoquak@gmail.com>
2017-02-17 14:41:43 +0100

    Nieuw schema gebaseerd op nieuw UML
    Kleine fixes op UML gedaan om schema te kunnen genereren:
    	- Hier en daar leeg documentatieveld toegevoegd om aantal
    foutmeldingen te verminderen
    	- Asdictionary op true gezet bij ThermalProductTypeIMKLValue
    	- Boolean --> Boolean op zes plaatsen
    	- CodeList Soortwerkzaamheden toegevoegd.
             - IMKLBasis als superklasse van Geraaktbelang verwijderd omdat
    dat al via superklasse Belang geregelgd was.

--- ShapeChange/RunShapeChange.bat
+++ ShapeChange/RunShapeChange.bat
@@ -1,4 +1,9 @@
-"C:\Program Files (x86)\java\jre8\bin\java" -jar D:\cquak\SURFdrive\ShapeChange\ShapeChange-2.1.0\ShapeChange-2.1.0.jar -Dfile.encoding=UTF-8 -c IMKL2015.xml
+rem "C:\Program Files (x86)\java\jre8\bin\java" -jar D:\cquak\SURFdrive\ShapeChange\ShapeChange-2.1.0\ShapeChange-2.1.0.jar -Dfile.encoding=UTF-8 -c IMKL2015.xml
+
+
+"C:\Program Files\Java\jre1.8.0_65\bin\java" -jar E:\SURFdrive\ShapeChange\ShapeChange-2.1.0\ShapeChange-2.1.0.jar -Dfile.encoding=UTF-8 -c IMKL2015.xml
+
 
 
 pause
+
